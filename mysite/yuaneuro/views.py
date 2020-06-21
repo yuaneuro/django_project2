afbce76af30a87fc510f676ce69c9bb1db8af9a2
yuaneuro <1280920616@qq.com>
2020-06-21 23:36:51 +0800

    调用github的api，并对调用的数据进行解析展示

--- mysite/yuaneuro/views.py
+++ mysite/yuaneuro/views.py
@@ -1,10 +1,9 @@
 from django.shortcuts import render
 import requests
-import random
+from random import randint
 
-# Create your views here.
 
 def home(request):
-    api = requests.get('https://api.github.com/users?since='+str(random.randint(0, 99999)))
+    api = requests.get('https://api.github.com/users?since=' + str(randint(0, 99999)))
     api_json = api.json()
     return render(request, 'home.html', {'api': api_json})
